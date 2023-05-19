6a97186924c8c3f8739699d955130f86a3e31a15
Gabriel Pelizzaro Pereira <gabrielpelizzaro@gmail.com>
2023-05-19 11:57:30 +0000

    create tensor.cc

--- lib/tensor.cc
+++ lib/tensor.cc
@@ -1,8 +1,13 @@
 #include <node_api.h>
+#include <torch/torch.h>
+#include <iostream>
 
 napi_value init(napi_env env, napi_value exports){
     napi_value findprimes;
 
+    torch::Tensor tensor = torch::rand({2, 3});
+    std::cout << tensor << std::endl;
+    
     return findprimes;
 }
 
