886340862b6cbab26533bf6d3b5bc006da1d5207
fullzer4 <gabrielpelizzaro@gmail.com>
2023-06-06 15:47:40 -0300

    new build

--- lib/tensor.cc
+++ lib/tensor.cc
@@ -1,14 +1,42 @@
-#include <node_api.h>
+#include <node/v8.h>
+#include <node/node.h>
 #include <torch/torch.h>
-#include <iostream>
 
-napi_value init(napi_env env, napi_value exports){
-    napi_value findprimes;
+namespace jstorch {
+  using v8::FunctionCallbackInfo;
+  using v8::Isolate; 
+  using v8::Local;
+  using v8::Object;
+  using v8::Number;
+  using v8::Value;
+  using v8::Exception;
+  using v8::String;
 
-    torch::Tensor tensor = torch::rand({2, 3});
-    std::cout << tensor << std::endl;
-    
-    return findprimes;
+  void Tensor(const FunctionCallbackInfo<Value>&args){
+    Isolate* isolate = args.GetIsolate();
+
+
+  }
+
+  void Net(const FunctionCallbackInfo<Value>&args){
+    Isolate* isolate = args.GetIsolate();
+
+    struct Net : torch::nn::Module {
+      Net() {
+        // tratar numero de camadas e de neuronios
+      }
+
+      torch::Tensor forward(torch::Tensor x) {
+        // tratar funcoes da rede
+        return x;
+      }
+    };
+  }
+ 
+  void Initialize(Local<Object> exports) {
+    NODE_SET_METHOD(exports, "tensor", Tensor);
+    NODE_SET_METHOD(exprots, "net", Tensor);
+  }
+
+  NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize);
 }
-
-NAPI_MODULE(NODE_GYP_MODULE_NAME, init);
