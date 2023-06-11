e16e971a4da235361d790b63508e21b12c3442e5
fullzer4 <gabrielpelizzaro@gmail.com>
2023-06-11 13:03:34 -0300

    tensor

--- lib/tensor.cc
+++ lib/tensor.cc
@@ -12,10 +12,53 @@ namespace jstorch {
   using v8::Exception;
   using v8::String;
 
-  void Tensor(const FunctionCallbackInfo<Value>&args){
+ void Tensor(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
 
+    if (args.Length() < 1) {
+      isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "É necessário pelo menos um argumento para criar o tensor.")));
+      return;
+    }
 
+    Local<Value> arg = args[0];
+    torch::Tensor tensor;
+
+    if (arg->IsArray()) {
+      Local<Array> array = Local<Array>::Cast(arg);
+      int64_t size = array->Length();
+
+      tensor = torch::zeros({size});
+      for (int64_t i = 0; i < size; i++) {
+        Local<Value> element = array->Get(i);
+        if (!element->IsNumber()) {
+          isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "O array deve conter apenas números.")));
+          return;
+        }
+        double value = element->NumberValue();
+        tensor[i] = value;
+      }
+    } else if (arg->IsObject()) {
+      Local<Object> obj = Local<Object>::Cast(arg);
+      int64_t size = obj->GetPropertyNames()->Length();
+
+      tensor = torch::zeros({size});
+      for (int64_t i = 0; i < size; i++) {
+        Local<Value> key = obj->GetProperty(i);
+        Local<Value> value = obj->Get(key);
+        if (!value->IsNumber()) {
+          isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "As propriedades do objeto devem conter apenas números.")));
+          return;
+        }
+        double numValue = value->NumberValue();
+        tensor[i] = numValue;
+      }
+    } else {
+      isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "O argumento deve ser um array ou um objeto.")));
+      return;
+    }
+
+    // Retornar o tensor criado
+    args.GetReturnValue().Set(/* Converter o tensor para um tipo JavaScript, se necessário */);
   }
 
   void Net(const FunctionCallbackInfo<Value>&args){
