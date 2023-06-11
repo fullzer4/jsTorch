cc8adaeecd2b4bf09585ea48a1d63f01c30ac5d8
fullzer4 <gabrielpelizzaro@gmail.com>
2023-06-11 02:37:49 -0300

    build

---
+++ setup.sh
@@ -0,0 +1,5 @@
+curl -O https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcpu.zip
+
+unzip libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcpu.zip
+
+rm -rf libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcpu.zip
